4ca138444f9b407dc49b678b569cdbbcaa251a32
Ekaterina Vaartis <vaartis@kotobank.ch>
2024-11-06 12:07:45 +0300

    Bump version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -96,7 +96,7 @@ defmodule PHash.MixProject do
   def project do
     [
       app: :phash,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
